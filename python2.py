5d8411f87b691857cb37fd937558de5b81baf152
jagagit90 <jagadees.praveen@gmail.com>
2021-11-02 21:33:17 +0530

    one file

---
+++ python2.py
@@ -0,0 +1,11 @@
+print('Starting Python tutorial')
+
+class student:
+    def __init__(self,val1,val2):
+        self.val1 = val1
+        self.val2 = val2
+    def addi(self):
+        return(self.val1+self.val2)
+
+stu1 = student(5,5)
+print(stu1.addi())
